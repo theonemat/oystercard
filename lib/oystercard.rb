c6179b0fc57b3b68326cf10593ff4c6df3f417bc
mat <matmoon14@gmail.com>
2019-03-11 15:20:12 +0000

    added balance + balance test

---
+++ lib/oystercard.rb
@@ -0,0 +1,10 @@
+class Oystercard
+
+  attr_reader :balance
+  def initialize
+    @balance = 0
+  end
+  # def add(amount)
+  #   @balance += amount
+  # end
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-end 
+  it 'checks money on card' do
+    expect(subject.balance).to eq 0
+  end
+  # it 'lets me add money' do
+  #   subject.add(10)
+  #   expect(subject.balance).to eq 10
+  # end
+end
